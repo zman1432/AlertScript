75d368f0ad58d84e72430b18e3045da01a52b429
Nathaniel Symer <fhsjaagshs@fhsjaagshs.com>
2012-02-20 18:49:04 -0500

    Fix many errors that held it back

--- AlertScript.m
+++ AlertScript.m
@@ -1,41 +1,28 @@
-#import <CoreFoundation/CoreFoundation.h>
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <libactivator/libactivator.h>
 #import <stdlib.h>
 
-@interface AlertScript : NSObject<LAListener, UIAlertViewDelegate, UITextViewDelegate>
-{ 
-@private
-  UIAlertView *av;
-  UITextView *scriptField;
-  NSString *script;
+@interface AlertScript : NSObject<LAListener, UIAlertViewDelegate> { 
+	UIAlertView *av;
+	UITextView *scriptField;
 }
 @end
 
 @implementation AlertScript
 
 - (BOOL)dismiss {
-  if (av) {	  
-    [av dismissWithClickedButtonIndex:[av cancelButtonIndex] animated:YES];
-    [scriptField release];
-    [script release];
-    [av release];
-    av = nil;
-    return YES;
-  } else {
-    return NO;
-  }
+	if (av) {	  
+    		[av dismissWithClickedButtonIndex:[av cancelButtonIndex] animated:YES];
+    		av = nil;
+    		return YES;
+ 	 } else {
+    		return NO;
+  	}
 }
 
-- (void)alertView:(UIAlertView *)alertView willDismissWithButtonIndex:(NSInteger)buttonIndex
-{
-    [av release];
-    av = nil;
-}
-
-- (void)activator:(LAActivator *)activator receiveEvent:(LAEvent *)event
-{
+- (void)activator:(LAActivator *)activator receiveEvent:(LAEvent *)event {
+	
 av = [[UIAlertView alloc] initWithTitle:@"AlertScript" message:@"\n\n\n\n" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Execute", nil];
 
 scriptField = [[UITextView alloc] initWithFrame:CGRectMake(30, 40, 223, 92)];
@@ -46,25 +33,27 @@ scriptField.autocorrectionType = UITextAutocorrectionTypeNo;
 scriptField.autocapitalizationType = UITextAutocapitalizationTypeNone;
 
 [av addSubview:scriptField];
-
 [av show]; 
+[av release];
+[scriptField release];
 
 [event setHandled:YES];
 }
 
-- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
-{
-  if (buttonIndex == 1)
-  {
-      script = scriptField.text;
-
-      [script writeToFile:@"/var/mobile/Library/AlertScript/AlertScript" atomically:YES encoding:NSUTF8StringEncoding error:nil];
-      system("AlertScriptMain >> /var/mobile/Library/AlertScript/stdlog.log 2>> /var/mobile/Library/AlertScript/stdlog.log");
-      UIAlertView *pwnMessage = [[[UIAlertView alloc] initWithTitle:@"Success" message:@"Your script has been executed." delegate:self cancelButtonTitle:@"Okay" otherButtonTitles:nil] autorelease];
-      [pwnMessage show];
-      [av release];
-      av = nil;
-  }
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+	if (buttonIndex == 1) {
+      		NSString *script = scriptField.text;
+      		[@"" writeToFile:@"/var/mobile/Library/AlertScript/stdlog.log" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+     		[script writeToFile:@"/var/mobile/Library/AlertScript/AlertScript" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+     		system("AlertScriptMain >> /var/mobile/Library/AlertScript/stdlog.log 2>> /var/mobile/Library/AlertScript/stdlog.log");
+     		NSString *output = [NSString stringWithContentsOfFile:@"/var/mobile/Library/AlertScript/stdlog.log" encoding:NSUTF8StringEncoding error:nil];
+     		NSString *message = [NSString stringWithFormat:@"Output:\n%@",output];
+     		[@"" writeToFile:@"/var/mobile/Library/AlertScript/stdlog.log" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+      		UIAlertView *pwnMessage = [[UIAlertView alloc] initWithTitle:@"Success" message:@"Your script has been executed." delegate:self cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+      		[pwnMessage show];
+      		[pwnMessage release];
+      		av = nil;
+	}
 }
 
 - (void)activator:(LAActivator *)activator abortEvent:(LAEvent *)event
@@ -74,7 +63,7 @@ scriptField.autocapitalizationType = UITextAutocapitalizationTypeNone;
 
 - (void)activator:(LAActivator *)activator otherListenerDidHandleEvent:(LAEvent *)event
 {
-	  [self dismiss];
+	[self dismiss];
 }
 
 - (void)activator:(LAActivator *)activator receiveDeactivateEvent:(LAEvent *)event
@@ -85,18 +74,15 @@ scriptField.autocapitalizationType = UITextAutocapitalizationTypeNone;
 
 - (void)dealloc
 {
-	[scriptField release];
 	[script release];
-	[av release];
-	[super dealloc];
 	[super dealloc];
 }
 
-+ (void)load
-{
-NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];	  
-[[LAActivator sharedInstance] registerListener:[self new] forName:@"com.fhsjaagshs.alertscript"];
-[pool release];
++ (void)load 
+{	
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];	  
+	[[LAActivator sharedInstance] registerListener:[self new] forName:@"com.fhsjaagshs.alertscript"];
+	[pool release];
 }
 
 @end
